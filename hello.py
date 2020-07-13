8e78a3296dd773467f9516c681549cac16af1506
Ivan Chokha <ivan_chokha@mail.ru>
2020-07-13 13:16:57 +0000

    complete module 1 on stepik

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ def app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return res = ["\n".join(environ.get('QUERY_STRING'.split("&")).encode('utf-8')]
+    res = ["\n".join(environ.get('QUERY_STRING').split("&")).encode('utf-8')]
+    return res
