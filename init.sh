8f6c1e6a028cf334e6ee6ba3d20aacabf75ecec7
Ivan Chokha <ivan_chokha@mail.ru>
2020-07-18 02:39:34 +0000

    prepare to complete module 2.1 task

--- init.sh
+++ init.sh
@@ -1,2 +1,14 @@
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /urs/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 hello:app &
+cd ./ask/ask
+gunicorn -b 0.0.0.0:8000 wsgi:application
+
