0175a9a098e2574e4aaed4329f3db1c4a99bce30
Ivan Chokha <ivan_chokha@mail.ru>
2020-07-25 12:41:00 +0000

    compete the lesson 2.3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,24 +2,24 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-class Question(models.Model):
-    objects = QuestionManager()
-    title = models.CharField(max_length = 255)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_new_add = True)
-    rating = models.IntegerField(default = 0)
-    author = models.ForiegnKey(User, on_delete = models.CASCADE)
-    likes = models.ManyToManyField(User, related_name = 'question_like_user')
-
-class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_new_add = True)
-    question = models.ForiegnKey(Question, on_delete = models.CASCADE)
-    author = models.ForiegnKey(User, on_delete = models.CASCADE)
-
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
 
     def popular(self):
         return self.order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length = 255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add = True)
+    rating = models.IntegerField(default = 0)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name = 'question_like_user')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add = True)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
