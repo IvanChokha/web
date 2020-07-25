7c67141d05dfb536c4995fb9fc3ce9cbaa6a80f6
Ivan Chokha <ivan_chokha@mail.ru>
2020-07-25 22:15:53 +1000

    add models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,11 +1,25 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Question(models.Model):
-    title = models.CharField(max_length=255)
+    objects = QuestionManager()
+    title = models.CharField(max_length = 255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_new_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForiegnKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
+    added_at = models.DateTimeField(auto_new_add = True)
+    rating = models.IntegerField(default = 0)
+    author = models.ForiegnKey(User, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name = 'question_like_user')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_new_add = True)
+    question = models.ForiegnKey(Question, on_delete = models.CASCADE)
+    author = models.ForiegnKey(User, on_delete = models.CASCADE)
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database stepik_web;"
-mysql -uroot -e "grant all privileges on setpik_web.* to 'box'@'localhost' with grant option;"
+mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
 python3 ~/web/ask/manage.py makemigrations
 python3 ~/web/ask/manage.py migrate
 cd ./ask/
