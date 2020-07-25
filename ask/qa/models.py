d99eb22fc2723502849b9ebe423c5a685f64de18
Ivan Chokha <ivan_chokha@mail.ru>
2020-07-25 09:26:01 +0000

    add first model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_new_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForiegnKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo nginx
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/task.py /etc/gunicorn.d/task.py
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepik_web;"
+mysql -uroot -e "grant all privileges on setpik_web.* to 'box'@'localhost' with grant option;"
+python3 ~/web/ask/manage.py makemigrations
+python3 ~/web/ask/manage.py migrate
+cd ./ask/
+python3 manage.py runserver 0.0.0.0:8000
